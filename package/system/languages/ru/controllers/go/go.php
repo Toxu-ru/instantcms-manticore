10bad3ef035ab1026361c798db52c6e5559ab95f
Evg <budo@narod.ru>
2020-02-03 01:03:59 +0300

    Create go.php

---
+++ package/system/languages/ru/controllers/go/go.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('LANG_GO_CONTROLLER',        'Поиск');
+    define('LANG_GO_TITLE',             'Поиск');
+    define('LANG_GO_ON',                'Поиск на %s');
+    define('LANG_GO_PERPAGE',           'Результатов на странице');
+    define('LANG_GO_GO',                 'Найти');
+    define('LANG_GO_TOO_SHORT',          'Слишком короткие слова в запросе');
+    define('LANG_GO_NO_RESULTS',         'Ничего не найдено');
+    define('LANG_GO_H1',                 'Результаты поиска по запросу «%s»');
