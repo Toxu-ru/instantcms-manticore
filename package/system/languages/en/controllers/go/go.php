81969808bc0a1469bab6ff1810bd85172d73d3b7
Evg <budo@narod.ru>
2020-02-03 02:16:56 +0300

    Update go.php

--- package/system/languages/en/controllers/go/go.php
+++ package/system/languages/en/controllers/go/go.php
@@ -5,6 +5,7 @@
     define('LANG_GO_ON',                'Search on %s');
     define('LANG_GO_PERPAGE',           'Results per page');
     define('LANG_GO_GO',                'Find');
+    define('LANG_GO_QUERY_INPUT',       'Enter your search term...');
     define('LANG_GO_TOO_SHORT',         'Too short search words');
     define('LANG_GO_NO_RESULTS',        'Nothing found');
     define('LANG_GO_H1',                'Search results for «%s»');
