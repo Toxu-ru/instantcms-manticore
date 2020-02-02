168b2c82993f07ba149e0c8877f04ea3efdb8201
Evg <budo@narod.ru>
2020-02-03 01:10:04 +0300

    Create go.php

---
+++ package/system/languages/en/controllers/go/go.php
@@ -0,0 +1,10 @@
+<?php
+
+    define('LANG_GO_CONTROLLER',        'Search');
+    define('LANG_GO_TITLE',             'Search');
+    define('LANG_GO_ON',                'Search on %s');
+    define('LANG_GO_PERPAGE',           'Results per page');
+    define('LANG_GO_GO',                'Find');
+    define('LANG_GO_TOO_SHORT',         'Too short search words');
+    define('LANG_GO_NO_RESULTS',        'Nothing found');
+    define('LANG_GO_H1',                'Search results for «%s»');
